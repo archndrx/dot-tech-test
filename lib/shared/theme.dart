92d9d5784af7385f04069bba1c758c392beaf2ad
Chandra <ardhyachandrax@gmail.com>
2024-03-27 06:14:05 +0700

    feat: add shared theme

---
+++ lib/shared/theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color blackColor = const Color(0xFF333333);
+Color grayColor = const Color(0xFF828282);
+Color whiteColor = const Color(0xffFFFFFF);
+
+TextStyle blackTextStyle = GoogleFonts.sourceSans3(
+  color: blackColor,
+);
+TextStyle grayTextStyle = GoogleFonts.sourceSans3(
+  color: grayColor,
+);
+TextStyle whiteTextStyle = GoogleFonts.sourceSans3(
+  color: whiteColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semibold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extrabold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
