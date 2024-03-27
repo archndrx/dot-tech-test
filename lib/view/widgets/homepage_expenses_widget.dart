8be2c73a8fc7ecda34d60e8acff391886b6e8a1e
Chandra <ardhyachandrax@gmail.com>
2024-03-27 07:17:32 +0700

    feat: add expenses widget

---
+++ lib/view/widgets/homepage_expenses_widget.dart
@@ -0,0 +1,51 @@
+import 'package:dot_tech_test/shared/theme.dart';
+import 'package:flutter/material.dart';
+
+class HomeExpenses extends StatelessWidget {
+  const HomeExpenses(
+      {super.key,
+      required this.color,
+      required this.text,
+      required this.nominal});
+
+  final Color color;
+  final String text;
+  final String nominal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(14),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: color,
+      ),
+      child: FittedBox(
+        alignment: Alignment.topLeft,
+        fit: BoxFit.scaleDown,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              text,
+              style: whiteTextStyle.copyWith(
+                fontSize: 14,
+                fontWeight: semibold,
+              ),
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+            Text(
+              "Rp. ${nominal}",
+              style: whiteTextStyle.copyWith(
+                fontSize: 18,
+                fontWeight: bold,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
